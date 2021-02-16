bd78962dda32985b8952b54466da15655276b91b
Le Yang <le.yang@gmail.com>
2021-02-16 15:15:29 -0500

    change retention days (#7)
    * change retention days
    
    * typo
    
    * abc
    
    * separate workflows
    
    Co-authored-by: Le Yang <lya236@uky.edu>

--- sql/src/SQL_STORED_PROCEDURE/dbo.zzz1.sql
+++ sql/src/SQL_STORED_PROCEDURE/dbo.zzz1.sql
@@ -17,29 +17,28 @@ CREATE OR ALTER PROCEDURE dbo.zzz1
 -- 05/02/2013	Keith Zomchek	55FP100014 - add @extraStatus to allow more status checks (ie PEND)
 -- 07/21/2014	Keith Zomchek	610009 - also return order ID (IDCode)
 --===================================================================
-
 (
   @clientguid		NUMERIC(16,0),
   @chartguid		NUMERIC(16,0),
   @clientvisitguid	NUMERIC(16,0),
   @ordername		VARCHAR(200),
-  @extraStatus		VARCHAR(200) = 'AUA1  go
-  '  --KAZ 55FP100014 - add @extraStatus, set default for backward compatibility and makes param optional
+  @extraStatus		VARCHAR(200) = 'AUA1  go'
 )
 AS
 BEGIN
-  SET ANSI_NULLS ON;
-  SET NOCOUNT ON;
+  SET ANSI_DEFAULTS ON;
+  SET CURSOR_CLOSE_ON_COMMIT OFF;
+  SET IMPLICIT_TRANSACTIONS OFF;
   SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
-SELECT TOP 1 o.GUID,o.IDCode  --KAZ 610009 - add o.IDCode
-FROM CV3Order o
- INNER JOIN CV3OrderCatalogMasterItem ocmi
- ON o.OrderCatalogMasterItemGUID = ocmi.GUID
- INNER JOIN CV3OrderStatus os
- ON o.OrderStatusCode = os.Code
- WHERE o.ClientGUID = @clientguid
- AND o.ClientVisitGUID = @clientvisitguid
- AND o.ChartGUID = @chartguid
- AND o.Active = 1
+  SELECT TOP 1 o.GUID,o.IDCode  --KAZ 610009 - add o.IDCode
+  FROM CV3Order o
+  INNER JOIN CV3OrderCatalogMasterItem ocmi
+  ON o.OrderCatalogMasterItemGUID = ocmi.GUID
+  INNER JOIN CV3OrderStatus os
+  ON o.OrderStatusCode = os.Code
+  WHERE o.ClientGUID = @clientguid
+  AND o.ClientVisitGUID = @clientvisitguid
+  AND o.ChartGUID = @chartguid
+  AND o.Active = 1
 END
